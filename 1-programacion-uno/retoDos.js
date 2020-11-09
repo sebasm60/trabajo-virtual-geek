c2abe837defd78a283dad4e78c29730e54363a48
sebasm60 <sebasg1325@hotmail.com>
2020-11-08 20:24:50 -0500

    1-programacion-uno

---
+++ 1-programacion-uno/retoDos.js
@@ -0,0 +1,8 @@
+'use strict';
+/*
+const address = 'Cr 79 N° 21 - 55 - Bello, Antioquia';
+address = 'Cr 67 N° 79b - 80 Medellin - Antioquia';
+No se puede cambiar el valor de addres ya que se declaro constante.
+*/
+let address = 'Cr 79 N° 21 - 55 - Bello, Antioquia';
+address = 'Cr 67 N° 79b - 80 Medellin - Antioquia';
